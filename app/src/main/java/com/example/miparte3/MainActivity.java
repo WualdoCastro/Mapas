d3cfc0664ec15baa4a0138e2903e9708094f6c17
Wualdo Castro <wualdo.castro@gmail.com>
2020-06-08 22:55:22 -0500

    Initial commit

---
+++ app/src/main/java/com/example/miparte3/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.miparte3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity
+        implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+    GoogleMap mapa;
+    LatLng ubicacion;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+// Obtenemos el mapa de forma asíncrona (notificará cuando esté listo)
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.mapa);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapa = googleMap;
+        ubicacion = new LatLng(-18.013766, -70.255331); //Nos ubicamos en la UNJBG
+        mapa.addMarker(new MarkerOptions().position(ubicacion).title("Marcador Tacna"));
+        mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(ubicacion,15));
+        mapa.setOnMapClickListener(this);
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            mapa.setMyLocationEnabled(true);
+        } else {
+            Button btnMiPos=(Button) findViewById(R.id.btnmiubi);
+            btnMiPos.setEnabled(false);
+        }
+    }
+    public void moveCamera(View view) {
+        mapa.moveCamera(CameraUpdateFactory.newLatLng(ubicacion));
+    }
+    public void addMarker(View view) {
+        Log.i("mipuntocentro", mapa.getCameraPosition().target.toString());
+        Log.i("mipuntocentro",String.valueOf( mapa.getCameraPosition().target.latitude));
+        Log.i("mipuntocentro",String.valueOf( mapa.getCameraPosition().target.longitude));
+        mapa.addMarker(new MarkerOptions().position(
+                mapa.getCameraPosition().target));
+    }
+    @Override public void onMapClick(LatLng puntoPulsado) {
+        Log.i("mipunto",puntoPulsado.toString());
+        Log.i("mipunto",String.valueOf(puntoPulsado.latitude));
+        Log.i("mipunto",String.valueOf(puntoPulsado.longitude));
+        mapa.addMarker(new MarkerOptions().position(puntoPulsado)
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+    }
+    public void ubicacion(View view) {
+        if (mapa.getMyLocation() != null)
+            mapa.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(mapa.getMyLocation().getLatitude(),
+                            mapa.getMyLocation().getLongitude()), 15));
+    }
+}
